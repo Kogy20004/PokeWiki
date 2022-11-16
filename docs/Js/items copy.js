b50380f01f6bc668ee43013a2b8a596658ee4a41
kogy <kogyisaza2004@gmail.com>
2022-11-15 23:20:22 -0500

    items funcional en su mayor parte

--- docs/Js/items copy.js
+++ docs/Js/items copy.js
@@ -1,28 +1,15 @@
 const container_card = document.getElementById("container_card");
 
 const fet_item = async (id) => {
-  for (let i = 1; i <= id; i++) {
-    if (i == 667 || i == 672 || i == 680 || i == 750) {
-      i = i + 1;
-    }
+  for (let i = 666; i <= id; i++) {
     const url = `https://pokeapi.co/api/v2/item/${i}/`;
     const res = await fetch(url);
-    const item = await res.json();
-    console.log(".");
-    if (item == "undefined") {
-      console.log(id);
+    if (res.ok) {
+      const item =  res.json();
+      console.log("ok");
     }
   }
 };
 
-const getitem = async (id) => {
-  const url = `https://pokeapi.co/api/v2/item/${id}/`;
-  const res = await fetch(url);
-  const item = await res.json();
-  console.log(".");
-  if (item == "undefined") {
-    console.log(id);
-  }
-};
-
-fet_item(2000);
+fet_item(1658); ;
+// 1658
--- docs/Js/items.js
+++ docs/Js/items.js
@@ -1,23 +1,40 @@
+const all_pokemon = document.getElementById("all_pokemon");
 const container_card = document.getElementById("container_card");
 
-const fet_item = async () => {
-  for (let i = 666; i <= 668; i++) {
-    if (i == 667) {
-      i++
-    }
+const fetch_item = async () => {
+  await create_nav_buttonsa();
+  const all_buttons = document.querySelectorAll(".button");
+  console.log(all_buttons);
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].disabled = true;
+  }
+
+  for (let i = 1; i <= 151; i++) {
     await getitem(i);
   }
+
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].disabled = false;
+  }
+  return all_buttons;
 };
 
 const getitem = async (id) => {
   const url = `https://pokeapi.co/api/v2/item/${id}/`;
   const res = await fetch(url);
-  const item = await res.json();
-  create_item_card(item);
-  console.log(item);
+  if (res.ok) {
+    const item = await res.json();
+    console.log("ok");
+
+    create_item_card(item);
+  }
 };
 
 function create_item_card(item) {
+  console.log(item);
+  if (item.sprites.default == null) {
+    return;
+  }
   const card = `
   <div class="card" id="pok_${item.id}">
     <div class="info_card">
@@ -35,10 +52,7 @@ function create_item_card(item) {
         </div>
         <div class="name_card">
           <h2>
-            ${
-              item.names[5].name.charAt(0).toUpperCase() +
-              item.names[5].name.slice(1)
-            }
+            ${name_card(item.names)}
           </h2>
         </div>
         <div class="Basic_data">
@@ -60,13 +74,100 @@ function create_item_card(item) {
   crear;
 }
 
-function description(x) {
-  for (let i = 0; i < x.length; i++) {
-    const element = x[i].language.name;
+function description(array) {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i].language.name;
     if (element == "es") {
-      return x[i].text;
+      return array[i].text;
+    }
+  }
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i].language.name;
+    if (element == "en") {
+      return array[i].text;
     }
   }
 }
 
-fet_item();
+function name_card(array) {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i].language.name;
+    console.log(element);
+    if (element == "es") {
+      return array[i].name;
+    }
+  }
+  for (let i = 0; i < array.length; i++) {
+    const element_v = array[i].language.name;
+    if (element_v == "en") {
+      return array[i].name;
+    }
+  }
+}
+
+all_pokemon.addEventListener("click", () => {
+  container_card.innerText = "";
+  fetch_item();
+});
+
+// nav
+
+const container_button_category = document.getElementById(
+  "container_button_category"
+);
+console.log(container_button_category);
+
+const create_nav_buttonsa = async () => {
+  for (let i = 1; i <= 8; i++) {
+    const url = `https://pokeapi.co/api/v2/item-pocket/${i}/`;
+    const res = await fetch(url);
+    const item_pocket = await res.json();
+    buttonHtml(item_pocket);
+  }
+  console.log("nav");
+};
+
+function buttonHtml(item_pocket) {
+  const button = `<button id="item-pocket_${item_pocket.id}" class="button list">${item_pocket.names[1].name}</button>`;
+  button;
+  const crear_button = container_button_category.insertAdjacentHTML(
+    "beforeend",
+    button
+  );
+  crear_button;
+  document
+    .getElementById(`item-pocket_${item_pocket.id}`)
+    .addEventListener("click", async () => {
+      const all_buttons = await document.querySelectorAll(".button");
+      console.log(all_buttons); 
+      container_card.innerText = "";
+      for (let i = 0; i < all_buttons.length; i++) {
+        all_buttons[i].disabled = true;
+      }
+      for (let i = 0; i < item_pocket.categories.length; i++) {
+        await categories_url(item_pocket.categories[i].url);
+      }
+      for (let i = 0; i < all_buttons.length; i++) {
+        all_buttons[i].disabled = false;
+      }
+    });
+}
+
+const categories_url = async (url) => {
+  const res = await fetch(url);
+  if (res.ok) {
+    const categories = await res.json();
+    for (let i = 0; i < categories.items.length; i++) {
+      await categories_items(categories.items[i].url);
+    }
+  }
+};
+const categories_items = async (url) => {
+  const res = await fetch(url);
+  if (res.ok) {
+    const item = await res.json();
+    create_item_card(item);
+  }
+};
+
+fetch_item();
